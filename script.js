8630e78c7a7adf9d421522a4eae5ede2442aab3a
PopoolaHakeem <hakeempopoola04@gmail.com>
2025-03-22 19:57:07 +0100

    updated index

--- script.js
+++ script.js
@@ -1,13 +1,13 @@
 
 var menuicon = document.querySelector("#menu-icon");
 var cancelicon = document.querySelector("#cancel-icon")
-var mobilenav = document.querySelector(".mobile-nav");
+var mobilenav = document.querySelector(".mobile-nav-list");
+var mobilenavbar = document.querySelector(".mobile-navbar")
 
 menuicon.onclick = function() {
     menuicon.style.display = 'none'
     cancelicon.style.display ='block'
     mobilenav.style.display = 'block'
-
 }
 
 cancelicon.onclick = function() {
