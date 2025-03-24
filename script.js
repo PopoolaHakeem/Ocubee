0ed58f58e4b39f1d5105fbab1288081bfea02094
PopoolaHakeem <hakeempopoola04@gmail.com>
2025-03-24 16:37:53 +0100

    updated index

--- script.js
+++ script.js
@@ -2,19 +2,59 @@
 var menuicon = document.querySelector("#menu-icon");
 var cancelicon = document.querySelector("#cancel-icon")
 var mobilenav = document.querySelector(".mobile-nav-list");
-var mobilenavbar = document.querySelector(".mobile-navbar");
+var navbarBg = document.querySelector(".ocube-navbar");
 
-menuicon.onclick = function() {
+// var mobilenavbar = document.querySelector(".mobile-navbar");
+// var nav = document.querySelector(".nav");
+
+// window.onscroll = function() {
+//     // if (window.scrollY > 300) {
+//     //    document.querySelector('h1').style.color="red"
+//     // }
+// }
+
+window.addEventListener('scroll', () => {
+    if (window.scrollY > 1) {
+        // navbar.style.
+        navbarBg.style.backgroundColor = 'white'
+        navbarBg.style.boxShadow = '0 2px 4px rgba(0, 0, 0, 0.3)'
+        // alert('hi')
+    } else {
+        navbarBg.style.backgroundColor = 'transparent'
+        navbarBg.style.boxShadow = 'none'
+
+        
+   }
+})
+
+
+menuicon.onclick = function () {
     menuicon.style.display = 'none'
     cancelicon.style.display ='block'
     mobilenav.style.display = 'block'
 }
 
-cancelicon.onclick = function() {
+cancelicon.onclick = function () {
     cancelicon.style.display = 'none'
     mobilenav.style.display = 'none'
     menuicon.style.display = 'block'
 }
-// mobilenavbar.scroll = function() {
-//     mobilenavbar.style.backgroundColor ='red'
-// }
+
+
+
+// document.body.addEventListener('', () => {
+//     // if (body.scrollY > 100) {
+//     //     alert("hello")
+//     // }
+
+//     alert('Hi')
+// })
+
+// window.addEventListener("scroll", function() {
+//     if (window.scrollY > 50 ){
+//         mobilenavbar.classList.add("nav")
+//     }else {
+//         mobilenavbar.classList.remove("nav")
+//     }
+
+// })
